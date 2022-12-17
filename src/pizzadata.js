f29911fbd59e61c444a3e841e7de0e822d9a4819
cmomie <cmomie1@gmail.com>
2022-12-17 14:53:51 -0600

    create react app and components

---
+++ src/pizzadata.js
@@ -0,0 +1,46 @@
+const pizzas = [
+    {
+        name: "Pepperion Supreme",
+        varients: ["small", "medium", "large"],
+        prices: [
+            {
+            small: 11,
+            medium: 13,
+            large: 16
+            }, 
+        ],
+        category: "nonveg",
+        image: "https://media.istockphoto.com/id/1192335992/photo/delicious-pizza-with-ham-and-mushrooms-on-white.jpg?s=612x612&w=0&k=20&c=VCGqDzJbyu4_2TNbqVZ0u7u2JGNCjdULDRC-4Zdt1t8=",
+        description: "Pepperoni Peppers Mozzarella Cheese",
+    },
+    {
+        name: "Veggie Supreme",
+        varients: ["small", "medium", "large"],
+        prices: [
+            {
+            small: 11,
+            medium: 13,
+            large: 16
+            }, 
+        ],
+        category: "nonveg",
+        image: "https://media.istockphoto.com/id/1192321079/photo/delicious-pizza-with-bacon-and-mushrooms-on-white.jpg?s=612x612&w=0&k=20&c=jgN66POVJEjjr5Pho8K5ygji3w1zU8kWrQyZMTYrIxw=",
+        description: "Mushrooms Peppers Tomatoes Mozzarella Cheese",
+    },
+    {
+        name: "Meat Supreme",
+        varients: ["small", "medium", "large"],
+        prices: [
+            {
+            small: 11,
+            medium: 13,
+            large: 16
+            }, 
+        ],
+        category: "nonveg",
+        image: "https://media.istockphoto.com/id/1192309553/photo/delicious-pizza-with-salami-on-white.jpg?s=612x612&w=0&k=20&c=eYwf3RJrlX__6Bs-0A-W1a9ReRtP3Z_YTTPagEbfymo=",
+        description: "Pepperoni Sausage Bacon Mozzarella Cheese",
+    },
+]
+
+export default pizzas
