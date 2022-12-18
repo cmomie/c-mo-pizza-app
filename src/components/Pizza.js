4bf8337d417e5feb446786b72715d5ea1ab3b223
cmomie <cmomie1@gmail.com>
2022-12-17 18:22:36 -0600

    Pizza File

--- src/components/Pizza.js
+++ src/components/Pizza.js
@@ -1,24 +1,35 @@
-import React from 'react'
+import React , {useState} from 'react' 
+import {Modal} from 'react-bootstrap'
 
 export default function Pizza({pizza}) {
+    const [ quantity , setquantity] = useState(1)
+    const [ varient , setvarient] = useState('small')
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
   return (
-    <div>
-      <h1>{pizza.name}</h1>
-      <img src={pizza.image} className="img-fluid" style={{height:'200px', width: '200px'}}/>
+    <div style={{margin: "70px"}} className="shadow-lg p-3 mb-5 bg-white rounded">
+        
+        <div onClick={handleShow}>
+        <h1>{pizza.name}</h1>
+        <img src={pizza.image} className="img-fluid" style={{height:'200px', width: '200px'}}/>
+        </div>
 
       <div className='flex-container'>
       
-        <div className='w-100'>
+        <div className='w-100 m-1'>
             <p>Varients</p>
-            <select>
+            <select className="form-control" value={varient} onChange={(e)=>{setvarient(e.target.value)}}>
                 {pizza.varients.map(varient=>{
                     return <option value={varient}>{varient}</option>
                 })}
             </select>
         </div>
-        <div className='w-100'>
+
+        <div className='w-100 m-1'>
             <p>Quantity</p>
-            <select>
+            <select className="form-control" value={quantity} onChange={(e)=>{setquantity(e.target.value)}}>
               {[...Array(10).keys()].map((x , i)=>{
                 
                 return <option value={i+1}>{i+1}</option>
@@ -28,6 +39,28 @@ export default function Pizza({pizza}) {
         </div>
 
       </div>
+      <div className='flex-container'>
+        <div className='m-1 w-100'>
+            <h1 className='mt-1'>Price : {pizza.prices[0][varient] * quantity}</h1>US/-
+        </div>
+        <div className='m-1 w-100'>
+            <button className="btn">ADD TO CART</button>
+        </div>
+
+      </div>
+      <Modal show={show}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal title</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <p>Modal body text goes here.</p>
+        </Modal.Body>
+
+        <Modal.Footer>
+              <button className='btn' onClick={handleClose}>CLOSE</button>
+        </Modal.Footer>
+      </Modal>
     </div>
   )
 }
